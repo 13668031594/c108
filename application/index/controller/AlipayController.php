ab7e0944a4a9a299bdddfc606b351d1254ba6743
yangyang <450428985@qq.com>
2019-03-24 18:38:21 +0800

    alipay

--- application/index/controller/AlipayController.php
+++ application/index/controller/AlipayController.php
@@ -55,6 +55,6 @@ class AlipayController extends \app\http\controller\IndexController
         //验证回调情况
         $this->classes->notify();
 
-        exit('支付成功');
+        parent::view('success');
     }
 }
