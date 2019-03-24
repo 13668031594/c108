c6dd47b7a9305424dc1652011b9fadacb0b88631
yangyang <450428985@qq.com>
2019-03-24 17:22:52 +0800

    alipay

--- application/index/controller/AlipayController.php
+++ application/index/controller/AlipayController.php
@@ -23,7 +23,7 @@ class AlipayController extends \app\http\controller\IndexController
 //        $this->classes->is_login();
 
         //自行下单
-        $param = $this->classes->create_order($request);
+//        $param = $this->classes->create_order($request);
 
         $param = (array)json_decode('{"body":"会员激活","subject":"会员激活","out_trade_no":"o1553416966638","total_amount":"0.01"}');
 
